3438795cae46168683cda9c9225f29e436c9a4c6
torii-ipt <torii@ipt.jp>
2025-11-26 18:54:55 +0900

    Update header.php

--- practiceWordPress/header.php
+++ practiceWordPress/header.php
@@ -9,17 +9,17 @@
     <meta name="keywords" content="英会話,ipt">
     <meta property="og:title" content="PracticeBBB英会話スクール">
     <meta property="og:description" content="24時間いつでも受講できる！オンライン対応の英会話スクールです。お気に入りの講師がいつでも選べます。無料体験実施中！">
-    <meta property="og:image" content="assets/img/mainvisual2.jpg">
+    <meta property="og:image" content="<?php echo get_template_directory_uri() ?>/assets/img/mainvisual2.jpg">
     <meta property="og:url" content="http://torii.jhkb-f.net/practice/">
     <meta property="og:type" content="website">
-    <link rel="icon" href="<?php echo get_template_directory_uri() ?>assets/img/favicon.ico">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>assets/reset.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>assets/style.css">
+    <link rel="icon" href="<?php echo get_template_directory_uri() ?>/assets/img/favicon.ico">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/assets/style.css">
 </head>
 
 <body>
     <header id="header" class="flex">
-        <h1 class="headerlogo"><a href="#"><img src="<?php echo get_template_directory_uri() ?>assets/img/logo-r.svg" alt="BBB英会話スクール"></a></h1>
+        <h1 class="headerlogo"><a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/logo-r.svg" alt="BBB英会話スクール"></a></h1>
         <nav id="gnav" class="flex">
             <ul class="menu flex">
                 <li class="menu-items"><a href="#reason">BBBが選ばれる理由</a></li>
