f2ee99a3db71512a9949dc8bf6ff4237bf51a38b
torii-ipt <torii@ipt.jp>
2025-11-27 17:23:02 +0900

    Update index.php

--- practiceWordPress/index.php
+++ practiceWordPress/index.php
@@ -1,3 +1,4 @@
+<?php get_header();  ?>
 <?php
     args = array(
         'posts_per_page' => -1,
@@ -10,7 +11,7 @@
                 <ul class="news-list">
                     <li class="news-list-items">
                         <a class="flex" href="<?php the_permalink(); ?>">
-                            <div class="time"><?php get_date(); ></div>
+                            <div class="time">2025.05.05</div>
                             <div class="news-category">category</div>
                             <div class="news-title"><?php the_title(); ?></div>
                         </a>
@@ -23,3 +24,4 @@
                         </a>
                     </li>
                     </ul>
+</div>
