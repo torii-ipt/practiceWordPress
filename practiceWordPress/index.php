eadc8ff634d1f525e0cddce954d3379f2e81257c
torii-ipt <torii@ipt.jp>
2025-11-27 17:14:54 +0900

    ignore

--- practiceWordPress/index.php
+++ practiceWordPress/index.php
@@ -1,103 +1,25 @@
-<?php get_header (); ?>
-    <main>
-        <div class="mv">
-            <ul class="mv-box">
-                <li><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mainvisual1.jpg" width="1920" height="1080" alt="mainvisual"></li>
-                <li><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mainvisual2.jpg" width="1920" height="1280" alt="mainvisual"></li>
-                <li><img src="<?php echo get_template_directory_uri(); ?>/assets/img/mainvisual3.jpg" width="1920" height="1280" alt="mainvisual"></li>
-            </ul>
-            <div class="mvtext flex">
-                <p>話して学ぼう！<br>BBB英会話スクール</p>
-                <a class="button01" href="#">無料体験はこちら</a>
-            </div>
-        </div>
-
-        <?php get_template_part('news'); ?>
-
-        <div id="reason">
-            <h2>BBBが選ばれる理由</h2>
-            <div class="reason-inner">
-                <div class="reason-innerbox flex slide-right">
-                    <div class="textbox">
-                        <p class="red">オンライン対応</p>
-                        <p>24時間いつでも受講できる！</p>
-                    </div>
-                </div>
-                <div class="reason-innerbox reverse flex slide-left">
-                    <div class="textbox">
-                        <p class="red">講師はネイティブ</p>
-                        <p>お気に入りの講師が自由に選べる！</p>
-                    </div>
-                </div>
-            </div><!-- /reason-container -->
-        </div>
-
-        <div id="interview" class="container">
-            <h2>受験生の声</h2>
-            <div class="comment flex">
-                <div class="person flex">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-student1.png" alt="大学生" width="240" height="240" loading="lazy">
-                    <p>○○○さん<br>大学生</p>
-                </div>
-                <p class="left">
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキスト
-                </p>
-            </div>
-
-            <div class="comment flex row-reverse">
-                <div class="person flex">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-student2.png" alt="会社員" width="240" height="240" loading="lazy">
-                    <p>○○○さん<br>大学生</p>
-                </div>
-                <p class="right">
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキスト
-                </p>
-            </div>
-            <div class="comment flex ">
-                <div class="person flex">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-student3.png" alt="自営業" width="240" height="240" loading="lazy">
-                    <p>○○○さん<br>大学生</p>
-                </div>
-                <p class="left">
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキスト<br>
-                    テキストテキストテキストテキストテキストテキストテキストテキストテキスト
-                </p>
-            </div>
-        </div>
-
-        <div id="about">
-            <div class="container">
-                <h2>スクールの概要</h2>
-                <ul class="about-list flex">
-                    <li class="about-card flex">
-                        <h3>レッスン内容</h3>
-                        <p class="en-ttl">lesson</p>
-                        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+<?php
+    args = array(
+        'posts_per_page' => -1,
+    );
+    posts = get_posts(args);
+?>
+<div class="news-wrap">
+            <div id="news" class="container">
+                <h2>NEWS</h2>
+                <ul class="news-list">
+                    <li class="news-list-items">
+                        <a class="flex" href="<?php the_permalink(); ?>">
+                            <div class="time"><?php get_date(); ></div>
+                            <div class="news-category">category</div>
+                            <div class="news-title"><?php the_title(); ?></div>
+                        </a>
                     </li>
-                    <li class="about-card flex">
-                        <h3>料金プラン</h3>
-                        <p class="en-ttl">price</p>
-                        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                    <li class="news-list-items">
+                        <a class="flex" href="<?php the_permalink(); ?>">
+                            <div class="time">2024.05.03</div>
+                            <div class="news-category">category</div>
+                            <div class="news-title">他のすべての HTML 要素と同様に、この要素はグローバル属性に対応しています。</div>
+                        </a>
                     </li>
-                    <li class="about-card flex">
-                        <h3>講師のご紹介</h3>
-                        <p class="en-ttl">teacher</p>
-                        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-                    </li>
-                    <li class="about-card flex">
-                        <h3>BBBのQ&A</h3>
-                        <p class="en-ttl">Q&A</p>
-                        <p>テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div id="apply" class="container">
-            <h2>まずは無料で、BBBの英会話を試してみませんか？</h2>
-            <p>今なら初月のレッスンを特別価格で受講できるクーポンをプレゼント！</p>
-            <a class="button01" href="#">無料体験に申し込む</a>
-        </div>
-    </main>
-<?php get_footer (); ?>
+                    </ul>
