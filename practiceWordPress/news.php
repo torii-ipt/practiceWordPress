e556672b6bae7174143d71f8551cd879fd831fd9
torii-ipt <torii@ipt.jp>
2025-12-02 17:58:03 +0900

    fix+α

--- practiceWordPress/news.php
+++ practiceWordPress/news.php
@@ -33,7 +33,7 @@
                 </ul>
 
                 <div class="news-btn">
-                    <a class="button02" href="#<?= get_site_url(); ?>/news">一覧はこちら</a>
+                    <a class="button02" href="<?= get_site_url(); ?>/news">一覧はこちら</a>
                 </div>
             </div>
         </div>
