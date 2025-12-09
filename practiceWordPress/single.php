760c3bde3414ae07f924df35b8c51fdb8f848551
torii-ipt <torii@ipt.jp>
2025-12-09 19:55:21 +0900

    try5

--- practiceWordPress/single.php
+++ practiceWordPress/single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main>
 
-        <div class="post">
+        <div class="news-list-items">
             <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
             <div class="time"><?php echo get_the_date(); ?></div>
@@ -24,3 +24,30 @@
 
 </main>
 <?php get_footer(); ?>
+
+<style>
+    .news-list-items .flex .news-category{
+    width: 5rem;
+    text-align: center;
+    font-weight: bold;
+    margin: 0 20px;
+    padding:  0 4px;
+    color: #FFF;
+    background-color: #ff2a2a;
+}
+
+.news-btn .button02{
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    color: #fff;
+    background-color: #ff2a2a;
+    box-shadow: 0px 3px 1px 0px #3b3b3b;
+    border-radius: 10px;
+    width: min(11vw,180px);
+    height: 40px;
+    font-size: clamp(16px, 1.42vw, 20px);
+    margin: 20px;
+    transition: 0.3s;
+}
+</style>
