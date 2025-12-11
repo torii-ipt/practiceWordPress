87788a9d9031e9216b87af5a8ba5e8257ebe7197
torii-ipt <torii@ipt.jp>
2025-12-11 19:17:14 +0900

    single.php

--- practiceWordPress/single.php
+++ practiceWordPress/single.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 <main>
 
-        <div class="news-list-items">
+        <section class="post-container container">
             <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
             <div class="time"><?php echo get_the_date(); ?></div>
-                            <div class="news-category">
+                            <div class="post-category">
                             <?php
                                 $categories = get_the_category();
                                 if (!empty($categories)) {
@@ -13,28 +13,19 @@
                                 }
                             ?>
                             </div>
-                            <div class="news-title"><?php echo get_the_title(); ?></div> 
-                            <div class="news-content"><?php the_content(); ?></div>
+                            <div class="post-title"><?php echo get_the_title(); ?></div> 
+                            <div class="post-content"><?php the_content(); ?></div>
             <?php endwhile; ?>
             <?php endif; ?>
-        </div>
+        </section>
         <div class="news-btn">
-            <a class="button02" href="<?= get_site_url(); ?>/news>">一覧はこちら</a>
+            <a class="button02" href="<?= get_site_url(); ?>/news">一覧はこちら</a>
         </div>
 
 </main>
 <?php get_footer(); ?>
 
 <style>
-    .news-list-items .flex .news-category{
-        text-align: center;
-        font-weight: bold;
-        margin: 0 20px;
-        padding:  0 4px;
-        color: #FFF;
-        background-color: #ff2a2a;
-    }
-
     .news-btn .button02{
         display: flex;
         justify-content:center;
